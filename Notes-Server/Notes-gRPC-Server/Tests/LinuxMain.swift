f2db6de3fbda433dbf296be43857d05a83a4e041
Tim Mewe <tim.mewe@me.com>
2019-12-12 14:46:23 +0100

    removes server tests

--- Notes-Server/Notes-gRPC-Server/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import Notes_gRPC_ServerTests
-
-var tests = [XCTestCaseEntry]()
-tests += Notes_gRPC_ServerTests.allTests()
-XCTMain(tests)
--- Notes-Server/Notes-gRPC-Server/Tests/Notes-gRPC-ServerTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(Notes_gRPC_ServerTests.allTests),
-    ]
-}
-#endif
