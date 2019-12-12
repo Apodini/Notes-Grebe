3d22c706d5d8b54d10dcde6ead59d5fa7e0e5fb9
Tim Mewe <tim.mewe@me.com>
2019-12-12 14:36:34 +0100

    adds app

---
+++ Notes-App/Notes-gRPC-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes-gRPC-App
+//
+//  Created by Tim Mewe on 12.12.19.
+//  Copyright © 2019 Tim Mewe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
